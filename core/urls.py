2e8603871cb085a8b4450bb8b705f4281a044d9c
mrsaliceselezneva <poluvesov.artyom@gmail.com>
2026-07-13 17:00:26 +0400

    Add first DRF API endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home
+from .views import home, game_info
 
 urlpatterns = [
     path("api/health/", home, name="health"),
+    path("api/game-info/", game_info)
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 def home(request):
@@ -6,3 +8,11 @@ def home(request):
         {"message": "Crocodile Maze backend работает"},
         json_dumps_params={"ensure_ascii": False},
     )
+
+
+@api_view(["GET"])
+def game_info(request):
+    return Response({
+        "name": "Лабиринт Крокодила",
+        "message": "API работает"
+    })
